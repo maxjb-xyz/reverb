b42c19d92e3642b47f47a42bb5327c8cede1ff6e
maxjb-xyz <max.barbare@maxjb.xyz>
2026-07-16 02:18:54 -0400

    feat(palette): cmd+k command palette

--- web/src/components/AppShell.tsx
+++ web/src/components/AppShell.tsx
@@ -10,6 +10,7 @@ import { MobileTabNav } from './MobileTabNav'
 import { MiniPlayer } from './MiniPlayer'
 import { NowPlayingOverlay } from './NowPlayingOverlay'
 import { CinemaView } from './CinemaView'
+import { CommandPalette } from './CommandPalette'
 import { Toaster } from './ui/Toaster'
 import { Skeleton } from './ui/Skeleton'
 import { useRealtime } from '../lib/realtimeWiring'
@@ -160,6 +161,7 @@ export function AppShell() {
       <MobileTabNav />
       <NowPlayingOverlay />
       <CinemaView />
+      <CommandPalette />
       <Toaster />
     </div>
   )
